67b31fc42d3c099b45c5f380e46b6a0b3e51c440
smuwjs <smuwjs@163.com>
2017-03-21 17:38:42 +0800

    Build project base architecture.

---
+++ mvp/src/main/java/com/jeeson/android/mvp/mvp/IModel.java
@@ -0,0 +1,10 @@
+package com.jeeson.android.mvp.mvp;
+
+/**
+ * Created by jess on 15/12/2016 10:45
+ * Contact with jess.yan.effort@gmail.com
+ */
+
+public interface IModel {
+    void onDestroy();
+}
---
+++ mvp/src/main/java/com/jeeson/android/mvp/mvp/Presenter.java
@@ -0,0 +1,9 @@
+package com.jeeson.android.mvp.mvp;
+
+/**
+ * Created by jess on 16/4/28.
+ */
+public interface Presenter {
+    void onStart();
+    void onDestroy();
+}
