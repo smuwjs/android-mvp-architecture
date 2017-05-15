ee535cc3b1bc377ead11ea1022bc13de9211d66d
Jeeson <smuwjs@163.com>
2017-05-15 18:03:24 +0800

    Create new branch 2.0

---
+++ mvp/src/main/java/me/jeeson/android/mvp/IModel.java
@@ -0,0 +1,8 @@
+package me.jeeson.android.mvp;
+
+/**
+ * Created by jeeson on 16/4/28.
+ */
+public interface IModel {
+    void onDestroy();
+}
---
+++ mvp/src/main/java/me/jeeson/android/mvp/IPresenter.java
@@ -0,0 +1,9 @@
+package me.jeeson.android.mvp;
+
+/**
+ * Created by jeeson on 16/4/28.
+ */
+public interface IPresenter {
+    void onStart();
+    void onDestroy();
+}
